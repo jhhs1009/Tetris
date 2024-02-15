a6269178aedda453ec18c95d38332a9feafdbec2
jhhs1009 <c6731105@gmail.com>
2024-02-15 23:05:26 +0900

    start 부분 만듬

--- Tetris/Tetris/test.cpp
+++ Tetris/Tetris/test.cpp
@@ -6,13 +6,47 @@ Start 화면 만들어 보겠음
 */
 
 #include <stdio.h>
+#include <iostream>
 
-void start()
+using namespace std;
+
+void start_logo()
 {
-	printf("========================\n");
+	printf("====================================================\n"
+		"                                                    \n"
+		"   ████████╗███████╗████████╗██████╗ ██╗███████╗    \n"
+		"   ╚══██╔══╝██╔════╝╚══██╔══╝██╔══██╗██║██╔════╝    \n"
+		"      ██║   █████╗     ██║   ██████╔╝██║███████╗    \n"
+		"      ██║   ██╔══╝     ██║   ██╔══██╗██║╚════██║    \n"
+		"      ██║   ███████╗   ██║   ██║  ██║██║███████║    \n"
+		"      ╚═╝   ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝╚══════╝    \n"
+		"                                                    \n"
+		"====================================================\n");
+	 
+
+}
+
+int start_str()
+{  
+	int startnum;
+	printf("\n                                      >> start (1) <<\n");
+	cout << "                                      >>  end (2)  <<\n";
+	cin >> startnum;
+	return startnum;
 }
 
 void main()
 {
-	start();
+	int startnum;
+	start_logo();
+	startnum = start_str();
+	printf("startnum : %d\n", startnum);
+
+	if (startnum == 2)
+	{
+		printf("Programe END");
+		return;
+	}
+	
+	cout << "테트리스 게임을 시작합니다.";
 }
